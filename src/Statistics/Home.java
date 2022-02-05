5f88a29878841f157dd2990b5c7d95ec88e557a7
HadeerEmadEldeen <hadeeremadalden123@gmail.com>
2022-02-05 23:52:23 +0200

    push project

---
+++ src/Statistics/Home.java
@@ -0,0 +1,13 @@
+package Statistics;
+
+public class Home {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws InterruptedException {
+
+        new HomeGui();
+    }
+
+}
---
+++ src/Statistics/Test.java
@@ -0,0 +1,26 @@
+package Statistics;
+
+
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Test {
+
+    public void test(String S, boolean ch) {
+        ArrayList<String> files = new ArrayList();
+        Read_dir R = new Read_dir();
+        files = R.getFiles(S,ch);
+        Read_runnable f1 = new Read_runnable();
+        f1.setfiles(files);
+        Thread t1 = new Thread(f1);
+        Thread t2 = new Thread(f1);
+        Thread t3 = new Thread(f1);
+        Thread t4 = new Thread(f1);
+        t1.start();
+        t2.start();
+        t3.start();
+        t4.start();
+        System.out.println(java.lang.Thread.activeCount());
+    }
+}
